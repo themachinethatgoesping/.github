04656efa1c8fa665d58b119e27ed0b12135dc710
Peter Urban <peter.urban@ugent.be>
2022-05-06 16:58:13 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'themachinethatgoesping'
+copyright = '2022, Ghent University'
+author = 'themachinethatgoesping authors'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
