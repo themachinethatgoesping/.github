147d279468ec1c7d37767e98666975aaf71a06f7
Peter Urban <peter.urban@ugent.be>
2022-05-10 11:34:23 +0200

    'adding logo to readthedocs'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_logo  = 'themachinethatgoesping.svg'
 html_theme_options = {
     'logo_only': False,
